09b9fd87940e10d93f310bf3e84e88c5d81494e5
nlpurcell <nlpurcell@crimson.ua.edu>
2023-02-28 15:23:04 -0600

    cleaned up orderController

--- ECE-POS-API/POS/src/main/java/com/capstone/POS/controllers/OrderController.java
+++ ECE-POS-API/POS/src/main/java/com/capstone/POS/controllers/OrderController.java
@@ -1,29 +1,17 @@
 package com.capstone.POS.controllers;
 
 import com.capstone.POS.exceptions.OrderNotFoundException;
-import com.capstone.POS.models.Item;
 import com.capstone.POS.models.Order;
-import com.capstone.POS.repositories.ItemRepository;
 import com.capstone.POS.repositories.OrderRepository;
 import com.capstone.POS.services.OrderService;
 
-import java.io.Console;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-
-import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/orders")
@@ -34,9 +22,6 @@ public class OrderController {
 
     @Autowired
     private OrderService orderService;
-
-    @Autowired
-    private ItemRepository itemRepository;
 
     @GetMapping
     public List<Order> getAllOrders() {
