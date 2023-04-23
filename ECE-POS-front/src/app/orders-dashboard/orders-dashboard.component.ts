59bfa3919e171365d18701d9c3a8d6625d2d7b08
jmruzicka <jmruzicka@crimson.ua.edu>
2023-04-23 11:21:39 -0500

    dashboard excel button

--- ECE-POS-front/src/app/orders-dashboard/orders-dashboard.component.ts
+++ ECE-POS-front/src/app/orders-dashboard/orders-dashboard.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { OrdersService } from '../orders.service';
 import { Order } from '../order';
 import { filter } from 'rxjs';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-orders-dashboard',
@@ -58,6 +59,21 @@ export class OrdersDashboardComponent implements OnInit {
       })
     
   }
+  }
+
+  exportexcel(): void
+  {
+    /* pass here the table id */
+    let element = document.getElementById('excel-table');
+    const ws: XLSX.WorkSheet =XLSX.utils.table_to_sheet(element);
+ 
+    /* generate workbook and add the worksheet */
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+ 
+    /* save to file */  
+    XLSX.writeFile(wb, "Group-Orders.xlsx");
+ 
   }
 
   computeStatus(order : Order) {
